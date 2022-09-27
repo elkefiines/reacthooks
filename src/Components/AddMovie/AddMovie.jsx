3853fa5d4828d11ee32b33759adc2b4de0266404
“elkefi <elkefiines28@gmail.com git config --global user.email elkefiines28@gmail.com>
2022-09-27 22:14:40 +0200

    react hooks checkpoint1

--- src/Components/AddMovie/AddMovie.jsx
+++ src/Components/AddMovie/AddMovie.jsx
@@ -1,51 +1,8 @@
 import React from "react";
-import "./AddMovie.css";
-
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 const AddMovie = () => {
-  return (
-    <div className="form">
-      <h3>Add a movie</h3>
-      <div className="form-body">
-        <div className="title">
-          <label className="form__label">Title :</label>
-          <input
-            className="form__input"
-            type="text"
-            placeholder=" Create title "
-          />
-        </div>
-        <div className="description">
-          <label className="form__label">Description:</label>
-          <input
-            type="text"
-            className="form__input"
-            placeholder="Create description "
-          />
-        </div>
-        <div className="posterURL">
-          <label className="form__label">PosterURL:</label>
-          <input
-            type="text"
-            className="form__input"
-            placeholder="Create posterURL "
-          />
-        </div>
-        <div className="rating">
-          <label className="form__label">Rating:</label>
-          <input
-            type="text"
-            className="form__input"
-            placeholder="Create rating "
-          />
-        </div>
-      </div>
-      <div className="footer">
-        <button type="submit" className="btn">
-          Add
-        </button>
-      </div>
-    </div>
-  );
+  return <div>AddMovie</div>;
 };
 
 export default AddMovie;
--- src/Components/AddPopup/AddPopup.jsx
+++ src/Components/AddPopup/AddPopup.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddPopup.css";
 
 const AddPopup = (props) => {
-  const handleAdd = () => {};
+  const [newMovie, setNewMovie] = useState({
+    title: "",
+    description: "",
+    posterURL: "",
+    rating: "",
+  });
+  const { title, description, posterURL, rating } = newMovie;
+  const handleChange = (e) => {
+    setNewMovie({
+      ...newMovie,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleAdd = () => {
+    console.log({ ...newMovie, id: Math.floor(Math.random() * 1000) });
+    props.addNewMovie({ ...newMovie, id: Math.floor(Math.random() * 1000) });
+    props.setShow(!props.show);
+  };
+
   return (
     <div className="popup-box">
       <div className="box">
-        <span className="close-icon" onClick={props.handleClose}>
+        <span className="close-icon" onClick={() => props.setShow(!props.show)}>
           x
         </span>
         <div className="form">
@@ -18,30 +36,43 @@ const AddPopup = (props) => {
                 className="form__input"
                 type="text"
                 placeholder=" Create title "
+                name="title"
+                value={title}
+                onChange={handleChange}
               />
             </div>
             <div className="description">
               <label className="form__label">Description:</label>
-              <input
-                type="text"
-                className="form__input"
-                placeholder="Create description "
-              />
+              <textarea
+                name="description"
+                cols="30"
+                rows="10"
+                value={description}
+                onChange={handleChange}
+              ></textarea>
             </div>
             <div className="posterURL">
               <label className="form__label">PosterURL:</label>
               <input
-                type="text"
+                name="posterURL"
+                type="url"
                 className="form__input"
                 placeholder="Create posterURL "
+                value={posterURL}
+                onChange={handleChange}
               />
             </div>
             <div className="rating">
               <label className="form__label">Rating:</label>
               <input
-                type="text"
+                name="rating"
+                type="number"
                 className="form__input"
                 placeholder="Create rating "
+                min="1"
+                max="5"
+                value={rating}
+                onChange={handleChange}
               />
             </div>
           </div>
